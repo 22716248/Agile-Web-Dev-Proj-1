4bcc4c8d827e5968ddd31770c8f5f7a31debcdbd
j-vanrav <j-vanrav@users.noreply.github.com>
2021-04-24 18:16:49 +0800

    flask login

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/index')
+def index():
     return render_template('index.html')
 
-@app.route('/example')
-def welcome():
-    return "Example page"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'testlogin' or request.form['password'] != 'testpassword':
+            error = "login details not allowed"
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error = error)
 
 if __name__=='__main__':
     app.run(debug=True)
