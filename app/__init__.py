1609df9f00c64d89cad0fccbc789373966a930c2
Adam301c <adam.chenyx@gmail.com>
2021-05-10 19:56:55 +0800

    Moves templates into app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,5 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask import render_template
-
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-from app import routes, models
-if __name__=='__main__':
-    app.run(debug=True)
+from app import routes
