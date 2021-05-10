ed8dfe8bbcac30099cb13d3984b6c627765b7cab
Adam301c <adam.chenyx@gmail.com>
2021-05-10 20:54:19 +0800

    Revert "Basic flask"
    This reverts commit 46946aa40e082fca685671ee87ce01fc822556dd.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+<<<<<<< HEAD
 from flask import render_template
+=======
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+>>>>>>> parent of 46946aa (Basic flask)
 app = Flask(__name__)
 
+<<<<<<< HEAD
 from app import routes
+=======
+from app import routes, models
+>>>>>>> parent of 46946aa (Basic flask)
