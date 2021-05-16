7d71d18697521884dc1907c1525853a090783e59
kuba <22719248@student.uwa.edu.au>
2021-05-16 17:16:40 +0800

    Quiz Implemented
    This implements the quiz with hardcoded questions. It uses POST and GET with Flask Forms, and basic server side validation. It updates the database, on a user-based, per attempt level.

--- app/forms.py
+++ app/forms.py
@@ -21,16 +21,16 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different username.')
 
 class QuizForm(FlaskForm):
-    question1 = StringField("Question 1: ", validators=[DataRequired()])
-    question2 = StringField("Question 2: ", validators=[DataRequired()])
-    question3 = StringField("Question 3: ", validators=[DataRequired()])
-    question4 = StringField("Question 4: ", validators=[DataRequired()])
-    question5 = StringField("Question 5: ", validators=[DataRequired()])
-    question6 = StringField("Question 6: ", validators=[DataRequired()])
-    question7 = StringField("Question 7: ", validators=[DataRequired()])
-    question8 = StringField("Question 8: ", validators=[DataRequired()])
-    question9 = StringField("Question 9: ", validators=[DataRequired()])
-    question10 = StringField("Question 10: ", validators=[DataRequired()])
+    question1 = StringField("Question 1: What constellation is the smallest of the 88 Modern constellations?", validators=[DataRequired()])
+    question2 = StringField("Question 2: On the flags of which countries can you find the Crux constellation?", validators=[DataRequired()])
+    question3 = StringField("Question 3: Which constellation represents the god Ea in the ancient Babylonian culture?", validators=[DataRequired()])
+    question4 = StringField("Question 4: Which constellation depicts a superhuman giant hunter?", validators=[DataRequired()])
+    question5 = StringField("Question 5: Which constellation contains the star system Alpha Centauri?", validators=[DataRequired()])
+    question6 = StringField("Question 6: Within what constellation did the brightest stellar event in recorded history occur?", validators=[DataRequired()])
+    question7 = StringField("Question 7: Which constellation is located near the galactic centre?", validators=[DataRequired()])
+    question8 = StringField("Question 8: Which constellation is regonised as the leaning coconut tree in oceanic cultures?", validators=[DataRequired()])
+    question9 = StringField("Question 9: Which constellation represents one of the larger dogs of Orion?", validators=[DataRequired()])
+    question10 = StringField("Question 10: What star system is closest to our sun?", validators=[DataRequired()])
 
     submit = SubmitField('Submit Answers.')
 
--- app/routes.py
+++ app/routes.py
@@ -4,106 +4,108 @@ from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, QuizForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Score
+from app.models import Question, User, Score
 from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
 
-@app.route('/quiz')
+@app.route('/quiz', methods=['POST', 'GET'])
 def quiz():
     form = QuizForm()
     if form.validate_on_submit:
         user_id = current_user.get_id()
         scores = []
+        attempts_list = Score.query.with_entities(Score.attempts).where(Score.user_id == user_id).all()
+        curr_attempt = max(attempts_list)[0] + 1
 
         # All questions hardcoded
 
         #Question 1
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question1.data == "Crux":
+            newScore = Score(user_id=user_id, score=1, question_id=1, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=1, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 2
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question2.data == "Australia, New Zealand, Brazil, Papua New Guinea, Samoa":
+            newScore = Score(user_id=user_id, score=1, question_id=2, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=2, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 3
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question3.data == "Aquarius":
+            newScore = Score(user_id=user_id, score=1, question_id=3, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=3, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 4
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question4.data == "Orion":
+            newScore = Score(user_id=user_id, score=1, question_id=4, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=4, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 5
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question5.data == "Centaurus":
+            newScore = Score(user_id=user_id, score=1, question_id=5, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=5, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 6
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question6.data == "Lupus":
+            newScore = Score(user_id=user_id, score=1, question_id=6, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=6, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 7
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question7.data == "Sagittarius":
+            newScore = Score(user_id=user_id, score=1, question_id=7, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=7, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 8
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question8.data == "Scorpius":
+            newScore = Score(user_id=user_id, score=1, question_id=8, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=8, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 9
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question9.data == "Canis Major":
+            newScore = Score(user_id=user_id, score=1, question_id=9, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=9, attempts=curr_attempt)
             scores.append(newScore)
 
         #Question 10
-        if form.question1.data == "answer":
-            newScore = Score(user_id=user_id, score=1) # Make DB add a new attempt each time? !<<<<<<<<<<<
+        if form.question10.data == "Alpha Centauri":
+            newScore = Score(user_id=user_id, score=1, question_id=10, attempts=curr_attempt)
             scores.append(newScore)
         else:
-            newScore = Score(user_id=user_id, score=0)
+            newScore = Score(user_id=user_id, score=0, question_id=10, attempts=curr_attempt)
             scores.append(newScore)
 
         db.session.add_all(scores)
         db.session.commit()
         #return redirect(url_for('index'))
-    return render_template('quiz.html', title='Take Quiz', form=form)
+    return render_template('quiz.html', title='Quiz', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
