7302003c46510e23ff1bd730f236b402bf42622c
Adam301c <adam.chenyx@gmail.com>
2021-05-16 21:04:43 +0800

    fixed bug when attempt = 0

--- app/routes.py
+++ app/routes.py
@@ -156,7 +156,8 @@ def user(username):
             attempts = max(attempts_list)[0]
             for i in list(range(attempts)):
                 score = Score.query.filter(and_(Score.user_id==user_id,Score.score==1,Score.attempts == i+1)).count()
-                scores.append({'body':score})
+                scor = "score" + "/10"
+                scores.append({'body':scor})
     else:
         scores=[{'body':"Have not attempted the quiz"}]
     
