b4879348a2f5fe2ef7c062344e9ab2e745e4afcc
de-ceased <chingeraldine828@yahoo.com>
2021-05-17 15:24:07 +0800

    Update tests.py
    Added /quiz tests

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest, os
 from app import app, db
-from app.models import User, Score
+from app.models import User, Score, Question
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -13,10 +13,12 @@ class UserModelCase(unittest.TestCase):
         u4 = User(id=2,username='Bob')
         u1.setpw('Hello')
         u2.setpw('World')
+        q1 = Question(id=0, constellation='Crux')
         # u3.setpw('1111')
         db.session.add(u1)
         db.session.add(u2)
         # db.session.add(u3)
+        db.session.add(q1)
         db.session.commit()
 
     def tearDown(self):
@@ -51,7 +53,7 @@ class UserModelCase(unittest.TestCase):
         self.assertNotEqual(u1.username, 'Test2')
         self.assertNotEqual(u2.username, 'Unit2')
 
-    # test user login works
+    # test user login works with parameters
     def test_user_login(self):
         u1 = User.query.get('0')
         response = self.app.post('/login', data=dict(
@@ -78,17 +80,17 @@ class UserModelCase(unittest.TestCase):
         response = self.app.get('/register', content_type='html/text') 
         self.assertEqual(response.status_code, 200)
 
-    # login page contains incorrect infomation/text
+    # register page contains incorrect infomation/text
     def test_registration_page_incorrect_info(self):
         response = self.app.get('/register', content_type='html/text')
         self.assertFalse(b'Welcome' in response.data)
 
-    # login page contains correct infomation/text
+    # register page contains correct infomation/text
     def test_registration_page_correct_info(self):
         response = self.app.get('/register', content_type='html/text')
         self.assertTrue(b'Register' in response.data)
 
-    # test user register page works
+    # test user register page works with parameters
     def test_user_register(self):
         u1 = User.query.get('0')
         response = self.app.post('/register', data=dict(
@@ -110,11 +112,38 @@ class UserModelCase(unittest.TestCase):
         response = self.app.get('/index', content_type='html/text')
         self.assertTrue(b'Constellation' in response.data)
     
-    # test index works, returns response OK 200
+    # test index page works, returns response OK 200
     def test_index_works(self):
         response = self.app.get('/index', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
+    # test quiz page works, returns response OK 200
+    def test_quiz(self):
+        response = self.app.get('/quiz', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    # quiz page contains correct infomation/text
+    def test_quiz_page_incorrect_info(self):
+        response = self.app.get('/quiz', content_type='html/text')
+        self.assertFalse(b'Welcome' in response.data)
+
+    # quiz page contains correct infomation/text
+    def test_quiz_page_correct_info(self):
+        response = self.app.get('/quiz', content_type='html/text')
+        self.assertTrue(b'QUIZ' in response.data)
+
+    # quiz works with request parameters
+    def test_quiz2(self):
+        q = Question.query.get('0')
+        response = self.app.post('/quiz', data=dict(
+            question = q.id, constellation = 'Crux'), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    # correct answer to quiz question
+    def test_quiz_correct_answer(self):
+        q = Question.query.get('0')
+        self.assertEqual(q.constellation, 'Crux')
+
 
     if __name__ == '__main__':
         unittest.main(verbosity=2)
